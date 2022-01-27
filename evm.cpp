5681e4c378e276a41346387344a0ab4ff48066e2
HieuPhan <mhieu25101998@gmail.com>
2022-01-27 15:35:46 +0700

    add call function

--- evm.cpp
+++ evm.cpp
@@ -133,10 +133,6 @@ DeployRs deploy(
       length_storage: (int)str_storage.size()
     };
 
-    myfile.open ("example.txt");
-    std::string stt(rs.b_storage, rs.length_storage);
-    myfile << stt;
-    myfile.close();
     memcpy( rs.b_exmsg, (char*)result.exmsg.c_str(), result.exmsg.size() );
     memcpy( rs.b_deployed_address, (char*)&b_contract_address, 20 );
     memcpy( rs.b_deployed_code, result.output.data(), str_storage.size() );
@@ -154,85 +150,65 @@ eevm::AccountState init_contract_with_storage(
   return env.gs.create_with_storage(contract_address, balance, code, j_storage);
 }
 
-// CallRs call(
-//   char* b_caller_address,
-//   char* b_caller_last_hash,
-//   char* b_contract_address,
-//   char* b_contract_balance,
-//   char* b_old_storage,
-//   int   length_old_storage,
-//   char* b_code,
-//   int   length_code,
-//   char* jb_input,
-//   int   length_input,
-// ) {
-//     // format argument to right data type
-//     uint256_t caller_address = eevm::from_big_endian((uint8_t *)b_caller_address, 20u);
-//     uint256_t caller_last_hash = eevm::from_big_endian((uint8_t *)b_caller_last_hash, 32u);
-//     uint256_t contract_address = eevm::from_big_endian((uint8_t *)b_contract_address, 20u);
-//     uint256_t contract_balance = eevm::from_big_endian((uint8_t *)b_contract_balance, 32u);
-//     std::vector<uint8_t> code((uint8_t*)b_code, (uint8_t*)b_code + code_length);
-//     std::vector<uint8_t> input((uint8_t*)b_input, (uint8_t*)b_input + input_length);
-//     std::string old_storage(b_old_storage, length_old_storage)
-//     nlohmann::json j_storage = nlohmann::json::parse((std::string)old_storage);
+CallRs call(
+  char* b_caller_address,
+  char* b_caller_last_hash,
+  char* b_contract_address,
+  char* b_contract_balance,
+  char* b_old_storage,
+  int   length_old_storage,
+  char* b_code,
+  int   length_code,
+  char* b_input,
+  int   length_input
+) {
+    // format argument to right data type
+    uint256_t caller_address = eevm::from_big_endian((uint8_t *)b_caller_address, 20u);
+    uint256_t caller_last_hash = eevm::from_big_endian((uint8_t *)b_caller_last_hash, 32u);
+    uint256_t contract_address = eevm::from_big_endian((uint8_t *)b_contract_address, 20u);
+    uint256_t contract_balance = eevm::from_big_endian((uint8_t *)b_contract_balance, 32u);
+    std::vector<uint8_t> code((uint8_t*)b_code, (uint8_t*)b_code + length_code);
+    std::vector<uint8_t> input((uint8_t*)b_input, (uint8_t*)b_input + length_input);
+    std::string old_storage(b_old_storage, length_old_storage);
+    nlohmann::json j_storage = nlohmann::json::parse((std::string)old_storage);
 
-//     eevm::SimpleGlobalState gs;
-//     //  init env
-//     Environment env{
-//             gs,
-//             caller_address,
-//             caller_last_hash,
-//             {}
-//     };
-//     // init contract and contract storage
-//     init_contract_with_storage(
-//             env,
-//             contract_address,
-//             contract_balance,
-//             code,
-//             j_storage
-//     );
-//     const auto result =
-//             run(env, caller_address, contract_address, input, jenv, jobj);
+    eevm::SimpleGlobalState gs;
+    //  init env
+    Environment env{
+            gs,
+            caller_address,
+            caller_last_hash,
+            {}
+    };
+    // init contract and contract storage
+    init_contract_with_storage(
+            env,
+            contract_address,
+            contract_balance,
+            code,
+            j_storage
+    );
+    const auto result =
+            run(env, caller_address, contract_address, input);
 
-//     // exitReason and exception and exmsg
-//     jbyte jb_exitReason = (jbyte)result.er;
-//     jbyte jb_exception = (jbyte)result.ex;
-//     jbyteArray jb_exmsg = jenv->NewByteArray(result.exmsg.size());
-//     jenv->SetByteArrayRegion( jb_exmsg, 0, result.exmsg.size(), (const jbyte*)result.exmsg.c_str() );
+    // storage
+    nlohmann::json json_storage = env.gs.get(contract_address).st.get_storage();
+    std::string str_storage = json_storage.dump();
 
-//     // storage
-//     nlohmann::json json_storage = env.gs.get(contract_address).st.get_storage();
-//     std::string str_storage = json_storage.dump();
-//     jbyteArray jb_storage = jenv->NewByteArray(str_storage.size());
-//     jenv->SetByteArrayRegion( jb_storage, 0, str_storage.size(), (const jbyte*)str_storage.c_str() );
-//     // output
-//     jbyteArray jb_output = jenv->NewByteArray(result.output.size());
-//     jenv->SetByteArrayRegion( jb_output, 0, result.output.size(), (const jbyte*)result.output.data() );
+    CallRs rs =  CallRs{
+      b_exitReason: (char)result.er,
+      b_exception: (char)result.ex,
+      b_exmsg: (char*) malloc((int)result.exmsg.size()*sizeof(char)),
+      b_output:  (char*) malloc((int)result.output.size()*sizeof(char)),
+      b_storage: (char*) malloc((int)str_storage.size()*sizeof(char)),
+    };
 
-//     jclass clazz = jenv->FindClass("com/example/nativec/EVMCallRs");
-//     jmethodID constructor = jenv->GetMethodID(clazz, "<init>", "(BB[B[B[B)V");
-//     // Create an instance of clazz
-//     jobject obj = jenv->NewObject(
-//             clazz,
-//             constructor,
-//             jb_exitReason,
-//             jb_exception,
-//             jb_exmsg,
-//             jb_output,
-//             jb_storage
-//     );
+    memcpy( rs.b_exmsg, (char*)result.exmsg.c_str(), result.exmsg.size() );
+    memcpy( rs.b_output, result.output.data(), str_storage.size() );
+    memcpy( rs.b_storage, str_storage.c_str(), str_storage.size() );
 
-//     jenv->ReleaseByteArrayElements(jb_caller_address, (jbyte*)b_caller_address, JNI_ABORT);
-//     jenv->ReleaseByteArrayElements(jb_caller_last_hash, (jbyte*)b_caller_last_hash, JNI_ABORT);
-//     jenv->ReleaseByteArrayElements(jb_contract_address, (jbyte*)b_contract_address, JNI_ABORT);
-//     jenv->ReleaseByteArrayElements(jb_contract_balance, (jbyte*)b_contract_balance, JNI_ABORT);
-//     jenv->ReleaseStringUTFChars(jstr_old_storage, old_storage);
-//     jenv->ReleaseByteArrayElements(jb_code, (jbyte*)b_code, JNI_ABORT);
-
-//     syslog(LOG_DEBUG, "Exit reason %d", jb_exitReason);
-//     return obj;
-// }
+    return rs;
+}
 
 
 
